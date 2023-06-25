3a5c6f79be63cc5a34c23b86c2e35b8834558953
Logan <loganoliveiraa23@alu.ufc.br>
2023-06-24 23:49:08 -0300

    primeiro

--- src/app/products.js
+++ src/app/products.js
@@ -8,12 +8,7 @@ export default function Home() {
           Conheça nossos produtos:&nbsp;
           <code className="font-mono font-bold">src/app/page.js</code>
         </p>
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        ></a>
+
 
 
       </div>
